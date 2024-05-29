795abf9d47b31d46fb4bd62164c8b3afeffb245d
Diego Prieto <diego_prieto@itrsa.com.ar>
2024-05-29 09:05:31 -0300

    agrega dominio y negocio

---
+++ webform/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+using negocio;
+
+namespace webform
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public List<Curso> Cursos { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
